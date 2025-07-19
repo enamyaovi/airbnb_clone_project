e06d4a9a5bbf54e46cd8dd43c33c7b5378897d35
Cephas Yaovi Tay <cephas.tay137@gmail.com>
2025-07-19 16:13:34 +0000

    edit

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,5 +1,5 @@
-from listings.models import Booking, Listing, CustomUser
 from rest_framework import serializers
+from listings.models import Booking, Listing, CustomUser
 from datetime import date
 from typing import Any
 
