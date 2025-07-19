0fecc230e049260a0cb762161d95cde483a65d03
Cephas Yaovi Tay <cephas.tay137@gmail.com>
2025-07-19 15:54:38 +0000

    modifed settings, new readme and renamed schema sql

--- alx_travel_app/listings/urls.py
+++ alx_travel_app/listings/urls.py
@@ -1,5 +1,9 @@
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from listings import views
 
@@ -10,5 +14,7 @@ router.register(r'bookings', views.BookingViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
