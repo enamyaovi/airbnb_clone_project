40dc70dc93fde42d4534adcd4b8550d016026437
Cephas Yaovi Tay <cephas.tay137@gmail.com>
2025-07-19 19:37:10 +0000

    file duplicates in listings

---
+++ alx_travel_app/alx_travel_app/listings/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+"""
+This is not the real serializer file please check the outer listing
+folder. for app files
+"""
+
+class DummySerializer(serializers.Serializer):
+    pass
