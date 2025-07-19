e9f3906416443ddd594535e74075f6b2a478ec1a
Cephas Yaovi Tay <cephas.tay137@gmail.com>
2025-07-19 16:01:58 +0000

    edit

--- alx_travel_app/alx_travel_app/listings/serializers.py
+++ alx_travel_app/alx_travel_app/listings/serializers.py
@@ -3,3 +3,7 @@
 This is not the real serializer file please check the outer listing
 folder. for app files
 """
+from rest_framework import serializers
+
+class DummySerializer(serializers.Serializer):
+    pass
